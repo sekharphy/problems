d2fa4234c2d893758c033634b40ea06f516fd972
sekharphy <152694003+sekharphy@users.noreply.github.com>
2024-06-16 21:05:55 +0530

    Add files via upload

---
+++ indoor.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 16 21:02:09 2024
+
+@author: sekha
+"""
+# printing the string for converting lower to upper viceversa
+z=input("enter string:\t")
+if z.islower():
+    print(z.upper())
+else:
+    print(z.lower())
